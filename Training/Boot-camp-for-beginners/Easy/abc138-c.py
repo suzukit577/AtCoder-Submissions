3747248d815975e289f9e4adf333af974b5d835c
suzukit577 <suzuki.t.dk@m.titech.ac.jp>
2024-05-21 07:15:58 +0900

    revised

--- Training/Boot-camp-for-beginners/Easy/abc138-c.py
+++ Training/Boot-camp-for-beginners/Easy/abc138-c.py
@@ -6,3 +6,12 @@ for _ in range(N - 1):
     z = (x + y) / 2
     V.append(z)
 print(*V)
+
+# ユーザ解説
+N = int(input())
+V = list(map(int, input().split()))
+V.sort()
+ans = V[0]
+for i in range(1, N):
+    ans = (ans + V[i]) / 2
+print(ans)
