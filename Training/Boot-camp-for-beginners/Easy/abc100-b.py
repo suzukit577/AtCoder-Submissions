8ae16173e042594b52a88bf65b75a09957e6f7d8
suzukit577 <suzuki.t.dk@m.titech.ac.jp>
2024-06-02 12:52:24 +0900

    revised

--- Training/Boot-camp-for-beginners/Easy/abc100-b.py
+++ Training/Boot-camp-for-beginners/Easy/abc100-b.py
@@ -1,1 +1,10 @@
 D, N = map(int, input().split())
+if D == 0:
+    nums = list(range(100)) + [101]
+    print(nums[N])
+if D == 1:
+    nums = list(range(0, 10000, 100)) + [10100]
+    print(nums[N])
+if D == 2:
+    nums = list(range(0, 1000000, 10000)) + [1010000]
+    print(nums[N])
