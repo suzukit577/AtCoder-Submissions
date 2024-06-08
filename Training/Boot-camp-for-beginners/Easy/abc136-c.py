8b5717f17ee1f43f3fe3f130a60fd603b34e12a6
suzukit577 <suzuki.t.dk@m.titech.ac.jp>
2024-06-08 20:59:22 +0900

    revised

--- Training/Boot-camp-for-beginners/Easy/abc136-c.py
+++ Training/Boot-camp-for-beginners/Easy/abc136-c.py
@@ -1,8 +1,7 @@
 N = int(input())
 H = list(map(int, input().split()))
 H[0] -= 1
-for i in range(1, N - 2):
-    if H[i - 1] <= H[i] - 1 <= H[i + 1]:
+for i in range(1, N - 1):
+    if H[i] - 1 >= H[i - 1]:
         H[i] -= 1
 print("Yes" if H == sorted(H) else "No")
-print(H)
