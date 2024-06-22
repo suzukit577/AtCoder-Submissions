c401052a39193ae355536bc6e42fff95c8983e5b
suzukit577 <suzuki.t.dk@m.titech.ac.jp>
2024-06-22 23:09:00 +0900

    revised

--- Algorithm/abc359/c.py
+++ Algorithm/abc359/c.py
@@ -1,6 +1,5 @@
-def minmax_horizontal_for_free(sx: int, sy: int, ty: int) -> tuple[int, int]:
-    pos = "left" if (sx + sy) % 2 == 0 else "right"
-    if pos == "left":
+def minmax_xcoord_for_free(sx: int, sy: int, ty: int) -> tuple[int, int]:
+    if (sx + sy) % 2 == 0:
         return sx - abs(sy - ty), sx + abs(sy - ty) + 1
     else:
         return sx - abs(sy - ty) - 1, sx + abs(sy - ty)
@@ -11,17 +10,11 @@ Tx, Ty = map(int, input().split())
 # 縦方向の移動は必ずコストがかかる
 ans = abs(Sy - Ty)
 # コストをかけずに移動できるマスの x 座標の最小値と最大値
-min_horizontal, max_horizontal = minmax_horizontal_for_free(Sx, Sy, Ty)
+min_x, max_x = minmax_xcoord_for_free(Sx, Sy, Ty)
 # 横方向の移動にコストがかかる場合を反映する
-if not min_horizontal <= Tx <= max_horizontal:
-    dx = 0
-    if Sx < Tx:
-        dx = Tx - max_horizontal
-        if dx % 2 == 1:
-            dx += 1
-    else:
-        dx = min_horizontal - Tx
-        if dx % 2 == 1:
-            dx += 1
+if not min_x <= Tx <= max_x:
+    dx = min(abs(Tx - min_x), abs(Tx - max_x))
+    if dx % 2 == 1:
+        dx += 1
     ans += dx // 2
 print(ans)
