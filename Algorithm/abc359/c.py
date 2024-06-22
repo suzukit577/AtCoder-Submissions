17c911d9998ffeefe3ed2278bdea51bdf93d674a
suzukit577 <suzuki.t.dk@m.titech.ac.jp>
2024-06-22 22:49:15 +0900

    revised

--- Algorithm/abc359/c.py
+++ Algorithm/abc359/c.py
@@ -8,9 +8,11 @@ def minmax_horizontal_for_free(sx: int, sy: int, ty: int) -> tuple[int, int]:
 
 Sx, Sy = map(int, input().split())
 Tx, Ty = map(int, input().split())
-ans = abs(Sy - Ty)  # 縦方向の移動は必ずコストがかかる
+# 縦方向の移動は必ずコストがかかる
+ans = abs(Sy - Ty)
+# コストをかけずに移動できるマスの x 座標の最小値と最大値
 min_horizontal, max_horizontal = minmax_horizontal_for_free(Sx, Sy, Ty)
-
+# 横方向の移動にコストがかかる場合を反映する
 if not min_horizontal <= Tx <= max_horizontal:
     dx = 0
     if Sx < Tx:
@@ -18,8 +20,7 @@ if not min_horizontal <= Tx <= max_horizontal:
         if dx % 2 == 1:
             dx += 1
     else:
-        min_cells = min_horizontal
-        dx = min_cells - Tx
+        dx = min_horizontal - Tx
         if dx % 2 == 1:
             dx += 1
     ans += dx // 2
