95d40e14fa02aba002a9d7c241b758de15de4d39
suzukit577 <suzuki.t.dk@m.titech.ac.jp>
2024-06-23 20:54:33 +0900

    revised

--- Algorithm/abc359/a.py
+++ Algorithm/abc359/a.py
@@ -1,3 +1,10 @@
 N = int(input())
 S = [input() for _ in range(N)]
 print(S.count("Takahashi"))
+
+# 別解
+N = int(input())
+ans = 0
+for _ in range(N):
+    ans += input() == "Takahashi"
+print(ans)
--- Algorithm/abc359/b.py
+++ Algorithm/abc359/b.py
@@ -5,3 +5,11 @@ for i in range(2, 2 * N):
     if A[i - 2] == A[i]:
         ans += 1
 print(ans)
+
+# evima 解説
+N = int(input())
+A = list(map(int, input().split()))
+ans = 0
+for i in range(len(A) - 2):
+    ans += A[i] == A[i + 2]
+print(ans)
