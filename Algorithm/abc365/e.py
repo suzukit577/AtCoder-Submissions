b255af5be352e881127b1ec77d9642befc3a7ec5
suzukit577 <suzuki.t.dk@m.titech.ac.jp>
2024-08-04 17:44:37 +0900

    revised

--- Algorithm/abc365/e.py
+++ Algorithm/abc365/e.py
@@ -1,2 +1,14 @@
+# evima 解説
 N = int(input())
 A = list(map(int, input().split()))
+L = 30
+
+ans = -sum(A)
+for p in range(L):
+    x = 0
+    c = [1, 0]
+    for a in A:
+        x ^= (a >> p) & 1
+        c[x] += 1
+    ans += c[0] * c[1] << p
+print(ans)
