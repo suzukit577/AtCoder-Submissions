806a781980a86b1dbbe545b7ab96d74229984f97
suzukit577 <suzuki.t.dk@m.titech.ac.jp>
2024-08-24 20:58:56 +0900

    revised

--- Algorithm/abc367/d.py
+++ Algorithm/abc367/d.py
@@ -4,8 +4,8 @@ A = list(map(int, input().split()))
 s = sum(A)
 A += A
 
-c = [0] * M
-d, ans = 0, 0
+c = [0] * M  # 距離が M の 0, 1, ..., M - 1 倍のペアの個数
+d, ans = 0, 0  # d: 累積和, ans: 条件を満たすペアの個数
 for i in range(2 * N):
     if i >= N:
         c[(d - s) % M] -= 1
