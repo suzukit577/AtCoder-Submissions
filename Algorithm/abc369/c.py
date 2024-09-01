6619bbc7d14ef8c5b5cb9a057cdc779bb8192266
suzukit577 <suzuki.t.dk@m.titech.ac.jp>
2024-09-02 07:26:33 +0900

    revised

--- Algorithm/abc369/c.py
+++ Algorithm/abc369/c.py
@@ -6,7 +6,9 @@ while left < N - 1:
         left == right or A[right + 1] - A[right] == A[left + 1] - A[left]
     ):
         right += 1
-        ans += right - left
+        ans += (
+            right - left
+        )  # 左端を A[left] に固定し、右端として A[left + 1], ..., A[right] のいずれかを選んで得られる部分列の個数
     else:
         left += 1
 print(ans)
@@ -14,6 +16,49 @@ print(ans)
 # 公式解説
 N = int(input())
 A = list(map(int, input().split()))
+B = [A[i + 1] - A[i] for i in range(N - 1)]
+
+from typing import Iterable, List, Tuple, TypeVar
+
+T = TypeVar("T")
+
+
+def run_length_encode(iterable: Iterable[T]) -> List[Tuple[T, int]]:
+    """与えられたイテラブルに対してランレングス圧縮を行う
+    （イテラブルの長さを N として、計算量は O(N)）
+
+    Args:
+        iterable (Iterable[T]): 任意のイテラブルなオブジェクト
+
+    Returns:
+        List[Tuple[T, int]]: 要素とその出現回数のペアのリスト
+    """
+    if not iterable:
+        return []
+
+    result = []
+    iterator = iter(iterable)
+    prev = next(iterator)
+    count = 1
+
+    for item in iterator:
+        if item == prev:
+            count += 1
+        else:
+            result.append((prev, count))
+            prev = item
+            count = 1
+
+    result.append((prev, count))  # 最後の要素を追加
+    return result
+
+
+B_enc = run_length_encode(B)
+ans = N
+for n, c in B_enc:
+    ans += c * (c + 1) // 2
+print(ans)
+
 
 # evima 解説
 N = int(input())
