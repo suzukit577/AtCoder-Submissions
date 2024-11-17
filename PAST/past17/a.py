988c7fd3337600fd461aee631d23f4cb43579664
suzukit577 <suzuki.t.dk@m.titech.ac.jp>
2024-11-18 00:14:39 +0900

    revised

--- PAST/past17/a.py
+++
@@ -1,9 +0,0 @@
-H, W = map(int, input().split())
-H /= 100
-BMI = W / (H**2)
-if BMI < 20:
-    print("A")
-elif BMI < 25:
-    print("B")
-else:
-    print("C")
--- PAST/past17/c.py
+++
@@ -1,7 +0,0 @@
-N = int(input())
-A = list(map(int, input().split()))
-P = list(map(int, input().split()))
-ans = 0
-for i in range(N):
-    ans += P[A[i] - 1]
-print(ans)
